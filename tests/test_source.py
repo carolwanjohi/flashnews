982a930fbb565388cf9e298d691d8987f1ae92c4
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-19 11:05:52 +0300

    Test Source class initialisation Create Test case for the initialisation of the Source class object

--- tests/test_source.py
+++ tests/test_source.py
@@ -13,10 +13,18 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method to run before each test case
         '''
-        self.news_source = Source('abc-news-au','ABC News (AU)', 'Most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.')
+        self.news_source = Source('abc-news-au','ABC News (AU)', 'Australia\'s most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.')
 
     def test_instance(self):
         '''
-        Test case to check if self.new_source is an instance of Source
+        Test case to check if self.news_source is an instance of Source
         '''
-        self.assertTrue( isinstance(self.news_source,Source))
+        self.assertTrue( isinstance( self.news_source, Source ) )
+
+    def test_init(self):
+        '''
+        Test case to check if the Source class is initialised
+        '''
+        self.assertEqual( self.news_source.id, 'abc-news-au')
+        self.assertEqual( self.news_source.name, 'ABC News (AU)')
+        self.assertEqual( self.news_source.description, 'Australia\'s most trusted source of local, national and world news. Comprehensive, independent, in-depth analysis, the latest business, sport, weather and more.')
