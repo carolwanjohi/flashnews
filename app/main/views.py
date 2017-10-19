2a0b23d78c1af9735bc825a36d6a46b12cc5c1a1
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-19 07:03:49 +0300

    Create title tags Create custom title tags for the index and source routes

--- app/main/views.py
+++ app/main/views.py
@@ -6,13 +6,15 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = "FlashNews"
     message = "Hello World"
-    return render_template('index.html', message=message)
+    return render_template('index.html', title=title, message=message)
 
-@main.route('/source/source')
-def source(source):
+@main.route('/source/<source_name>')
+def source(source_name):
     '''
-    View source oage function that returns a source page and its data
+    View source page function that returns a source page and its data
     '''
-    message = f"You are viewing {source}"
-    return render_template('source.html', message=message)
+    title = f"{source_name} Page"
+    message = f"You are viewing {source_name}"
+    return render_template('source.html', title=title, message=message)
