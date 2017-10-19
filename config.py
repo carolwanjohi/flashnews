6c82163bbfe8fdeb9672bd5c49c8531a99b30925
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-10-19 12:40:52 +0300

    Remove Secret Key Remove Secret Key from configurations of the application

--- config.py
+++ config.py
@@ -4,12 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    # MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    pass
-
-
 
 class ProdConfig(Config):
     '''
